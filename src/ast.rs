d1cd2f58dbf678f5c7be194fb345e85f16b18da7
John Irle <irlejohn@gmail.com>
2026-04-15 02:53:21 -0500

    feat: wrap identifier and string function

--- src/ast.rs
+++ src/ast.rs
@@ -5,29 +5,34 @@ use crate::token::Token;
 
 pub trait Node {
     fn token_literal(&self) -> String;
+    fn string(&self) -> String;
 }
 
 pub struct LetStatement {
     pub token: Token,
-    pub name: Identifier,
-    pub value: Expression,
+    pub name: Expression,
+    pub value: Option<Expression>,
 }
 
 impl Node for LetStatement {
     fn token_literal(&self) -> String {
         self.token.literal.clone()
     }
-}
 
-#[derive(Debug, Clone)]
-pub struct Identifier {
-    pub token: Token,
-    pub value: String,
-}
+    fn string(&self) -> String {
+        let mut out = String::new();
+        out.push_str(&self.token_literal());
+        out.push(' ');
+        out.push_str(&self.name.string());
+        out.push_str(" = ");
 
-impl Node for Identifier {
-    fn token_literal(&self) -> String {
-        self.token.literal.clone()
+        if let Some(value) = &self.value {
+            out.push_str(&value.string());
+        }
+
+        out.push(';');
+
+        out
     }
 }
 
@@ -41,11 +46,45 @@ impl Node for ReturnStatement {
     fn token_literal(&self) -> String {
         self.token.literal.clone()
     }
+
+    fn string(&self) -> String {
+        let mut out = String::new();
+
+        out.push_str(&self.token_literal());
+        out.push(' ');
+
+        if let Some(return_value) = &self.return_value {
+            out.push_str(&return_value.string());
+        }
+
+        out
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct ExpressionStatement {
+    token: Token,
+    expression: Option<Expression>,
+}
+
+impl Node for ExpressionStatement {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+
+    fn string(&self) -> String {
+        if let Some(expression) = &self.expression {
+            return expression.string();
+        }
+
+        String::new()
+    }
 }
 
 pub enum Statement {
     Let(LetStatement),
     Return(ReturnStatement),
+    Expression(ExpressionStatement),
 }
 
 impl Node for Statement {
@@ -53,13 +92,51 @@ impl Node for Statement {
         match self {
             Self::Let(s) => s.token_literal(),
             Self::Return(s) => s.token_literal(),
+            Self::Expression(s) => s.token_literal(),
         }
+    }
+
+    fn string(&self) -> String {
+        match self {
+            Self::Let(s) => s.string(),
+            Self::Return(s) => s.string(),
+            Self::Expression(s) => s.string(),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Identifier {
+    pub token: Token,
+    pub value: String,
+}
+
+impl Node for Identifier {
+    fn token_literal(&self) -> String {
+        self.token.literal.clone()
+    }
+
+    fn string(&self) -> String {
+        self.value.clone()
     }
 }
 
 #[derive(Debug, Clone)]
 pub enum Expression {
     Identifier(Identifier),
+}
+
+impl Node for Expression {
+    fn token_literal(&self) -> String {
+        match self {
+            Self::Identifier(identifier) => identifier.token_literal(),
+        }
+    }
+    fn string(&self) -> String {
+        match self {
+            Self::Identifier(identifier) => identifier.string(),
+        }
+    }
 }
 
 pub struct Program {
@@ -72,5 +149,34 @@ impl Node for Program {
             return self.statements[0].token_literal();
         }
         String::new()
+    }
+
+    fn string(&self) -> String {
+        self.statements.iter().map(Node::string).collect::<String>()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::token::TokenType;
+
+    #[test]
+    fn test_string() {
+        let program = Program {
+            statements: vec![Statement::Let(LetStatement {
+                token: Token::new(TokenType::Let, "let".into()),
+                name: Expression::Identifier(Identifier {
+                    token: Token::new(TokenType::Ident, "myVar".into()),
+                    value: "myVar".to_string(),
+                }),
+                value: Some(Expression::Identifier(Identifier {
+                    token: Token::new(TokenType::Ident, "anotherVar".into()),
+                    value: "anotherVar".to_string(),
+                })),
+            })],
+        };
+
+        assert_eq!(program.string(), "let myVar = anotherVar;");
     }
 }
